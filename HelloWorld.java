5bf3a57f95fc5df53df034a6f4c4bd4733006c2e
meghana202006 <meghanarodd20@gmail.com>
2025-04-19 18:12:19 +0530

    New Java statement2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
   public static void main(String args[]) {
     System.out.println("Hello World!");
     System.out.println("This is a new statement");
+    System.out.println("This my first git and github experience");
 
   }
 
